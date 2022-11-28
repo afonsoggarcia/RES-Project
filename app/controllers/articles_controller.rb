85627b643edb75d4f75a75bdc47c63dae496f098
Felipe Kummer <felipe.s.kummer@gmail.com>
2022-11-28 16:58:41 -0300

    pundit and articles policies done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
 class ArticlesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home index show]
   def index
+    skip_policy_scope
   end
 
   def show
+    skip_authorization
   end
 end
