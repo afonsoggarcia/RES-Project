26b35b801d93ce34741038e9e9bf0bf50fad7cdd
Felipe Kummer <felipe.s.kummer@gmail.com>
2022-11-28 17:34:23 -0300

    login page up and running

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit::Authorization
 
   # Pundit: allow-list approach
@@ -12,6 +13,15 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :believer, :description])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :believer, :description])
+  end
+
   private
 
   def skip_pundit?
