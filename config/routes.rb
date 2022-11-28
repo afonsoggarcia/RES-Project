3d7315941bb8df3c139a3ba6abf82a8d52975885
Afonso Garcia <afonso.guerra.garcia@gmail.com>
2022-11-28 17:14:56 -0300

    Article route updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
   devise_for :users
   root to: "pages#home"
   get "about", to: "pages#about"
+  resources :articles
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
