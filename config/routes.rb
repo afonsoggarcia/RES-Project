e4a33445e89c0ec40e0027e6e4544dd1e72ff267
Afonso Garcia <afonso.guerra.garcia@gmail.com>
2022-11-28 17:24:45 -0300

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "about", to: "pages#about"
   resources :articles
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
