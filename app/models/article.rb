aa2e03f23a64b06bcf8a63d4cee28bc98570e7b4
Daniel Velazquez <lorddaniot@gmail.com>
2022-11-29 15:25:51 -0300

    wip

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,5 @@ class Article < ApplicationRecord
   has_many :likes, dependent: :destroy
   validates :title, :subtitle, :content, presence: true
   has_one_attached :photo
+  has_rich_text :rich_body
 end
