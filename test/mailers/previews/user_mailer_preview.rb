db9f0ca60c5fe3a219a1ea4f8511402627b9192e
Afonso Garcia <afonso.guerra.garcia@gmail.com>
2022-11-30 16:04:16 -0300

    Mailer created

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
+  def welcome
+    user = User.last
+    # This is how you pass value to params[:user] inside mailer definition!
+    UserMailer.with(user: user).welcome
+  end
+end
