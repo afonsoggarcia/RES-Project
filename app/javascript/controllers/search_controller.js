4e4542581237762689e933a337ea456616323be6
Afonso Garcia <afonso.guerra.garcia@gmail.com>
2022-12-05 13:59:35 -0300

    Stimulus and Turbo added to search

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="search"
+export default class extends Controller {
+  connect() {
+    console.log('oi')
+  }
+
+  perform() {
+    this.element.requestSubmit();
+  }
+}
