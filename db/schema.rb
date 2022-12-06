d1b984f99bf64dbbec453f68906fd4cebd090a4f
Felipe Kummer <felipe.s.kummer@gmail.com>
2022-12-06 12:05:27 -0300

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_182436) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_124042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,14 +73,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_182436) do
     t.datetime "updated_at", null: false
   end
 
-
-
   create_table "chatrooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.bigint "reply_id", null: false
@@ -148,6 +143,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_182436) do
     t.boolean "publisher", default: false
     t.string "description"
     t.boolean "inactive?", default: false
+    t.boolean "converted", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
