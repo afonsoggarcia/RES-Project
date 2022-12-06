3806503a5462cee48b8b7828f1f4f98f62668afd
Daniel Velazquez <lorddaniot@gmail.com>
2022-12-06 13:27:02 -0300

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema[7.0].define(version: 2022_12_05_182436) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,14 +73,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_182436) do
     t.datetime "updated_at", null: false
   end
 
-
-
   create_table "chatrooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.bigint "reply_id", null: false
