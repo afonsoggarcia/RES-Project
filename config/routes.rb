c9bc60ba3823369cb4aacdf4cb8cc5ac3e960e29
Felipe Kummer <felipe.s.kummer@gmail.com>
2022-12-07 12:42:13 -0300

    journey finished

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
+  resources :requests, only: [:create, :update]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
